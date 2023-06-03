69d8d967cd1accf571b6515d358fcebe7f65dce6
aminulislam2002 <aminulislamemon2002@gmail.com>
2023-06-03 07:30:05 -0700

    show add to cart count on navbar

--- src/pages/Shared/NavBar/NavBar.jsx
+++ src/pages/Shared/NavBar/NavBar.jsx
@@ -2,9 +2,11 @@ import { useContext } from "react";
 import { Link } from "react-router-dom";
 import { FaShoppingCart } from "react-icons/fa";
 import { AuthContext } from "../../../providers/AuthProvider";
+import useCart from "../../../hooks/useCart";
 
 const NavBar = () => {
   const { user, logOut } = useContext(AuthContext);
+  const [cart] = useCart();
 
   const handleLogOut = () => {
     logOut()
@@ -32,7 +34,7 @@ const NavBar = () => {
         <Link to="/dashboard/mycart">
           <button className="btn gap-2">
             <FaShoppingCart></FaShoppingCart>
-            <div className="badge badge-secondary">+{0}</div>
+            <div className="badge badge-secondary">+{cart?.length || 0}</div>
           </button>
         </Link>
       </li>
